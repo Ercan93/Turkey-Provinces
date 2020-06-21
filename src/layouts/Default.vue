dba14453cf7c25e035d4bed0645c06991600c899
Ercan Uzunsakal <ercan112358@gmail.com>
2020-06-21 23:23:24 +0300

    Base modified.

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -4,12 +4,9 @@
       <strong>
         <g-link to="/">{{ $static.metadata.siteName }}</g-link>
       </strong>
-      <nav class="nav">
-        <g-link class="nav__link" to="/">Home</g-link>
-        <g-link class="nav__link" to="/about/">About</g-link>
-      </nav>
+      <nav class="nav"></nav>
     </header>
-    <slot/>
+    <slot />
   </div>
 </template>
 
@@ -23,9 +20,10 @@ query {
 
 <style>
 body {
-  font-family: -apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif;
-  margin:0;
-  padding:0;
+  font-family: -apple-system, system-ui, BlinkMacSystemFont, "Segoe UI", Roboto,
+    "Helvetica Neue", Arial, sans-serif;
+  margin: 0;
+  padding: 0;
   line-height: 1.5;
 }
 
